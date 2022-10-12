6d73105753eaf9303dcb7d54b3b8b68a2916177f
Engiineeer <ntpzlwo@gmail.com>
2022-10-12 19:09:19 +0300

    finished

--- products/views.py
+++ products/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from products.models import Product
+
+
 def get_home(request):
     return HttpResponse("<h1>Hello, World!</h1>")
-# Create your views here.
+
+
+def get_product(request, product_id):
+    product = Product.objects.get(id=product_id)
+    return HttpResponse(
+        f"<h1>product name {product.name} and price{product.price}</h1>"
+    )
--- productshop/urls.py
+++ productshop/urls.py
@@ -16,5 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from products import views
+from products.views import get_product
 
-urlpatterns = [path("admin/", admin.site.urls), path("home/", views.get_home)]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("home/", views.get_home),
+    path("products/<int:product_id>", views.get_product),
+]
