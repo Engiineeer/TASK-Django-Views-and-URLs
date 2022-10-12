a3bd5e95a358e422230049ef70fcca86b719ce53
Engiineeer <ntpzlwo@gmail.com>
2022-10-12 18:46:00 +0300

    home view

--- productshop/urls.py
+++ productshop/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from products import views
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-]
+urlpatterns = [path("admin/", admin.site.urls), path("home/", views.get_home)]
